a4ac255170201e4b329c50f67519b85c413b5f44
Liron Mizrahi <liri25112003@gmail.com>
2023-02-05 15:12:00 +0200

    build(convert button) Adding functionality

--- js/scripts.js
+++ js/scripts.js
@@ -42,32 +42,39 @@ window.addEventListener("DOMContentLoaded", (event) => {
 });
 
 var canvas = document.getElementById("myCanvas");
-const dropZone = document.querySelector('.drop-zone');
-const button = document.querySelector('.btn-primary');
-const ctx = canvas.getContext('2d');
+const dropZone = document.querySelector(".drop-zone");
+const button = document.querySelector(".btn-primary");
+const ctx = canvas.getContext("2d");
+
 let imageData;
 
-dropZone.addEventListener('dragover', (e) => {
+const btnConvert = document.getElementById("btn-convert");
+const canvasOutput = document.querySelector(".outputCanvas");
+const ctxOutput = canvasOutput.getContext("2d");
+let imageDataOutput;
+
+dropZone.addEventListener("dragover", (e) => {
     e.preventDefault();
-    dropZone.classList.add('drag-over');
+    dropZone.classList.add("drag-over");
 });
 
-dropZone.addEventListener('dragleave', (e) => {
+dropZone.addEventListener("dragleave", (e) => {
     e.preventDefault();
-    dropZone.classList.remove('drag-over');
+    dropZone.classList.remove("drag-over");
 });
 
-dropZone.addEventListener('drop', (e) => {
+dropZone.addEventListener("drop", (e) => {
     e.preventDefault();
-    dropZone.classList.remove('drag-over');
+    dropZone.classList.remove("drag-over");
 
     const file = e.dataTransfer.files[0];
     const reader = new FileReader();
 
-    reader.addEventListener('load', () => {
+    reader.addEventListener("load", () => {
         imageData = reader.result;
         const image = new Image();
         image.src = imageData;
+        imageDataOutput = processImage(imageData);
         image.onload = () => {
             canvas.width = image.width;
             canvas.height = image.height;
@@ -78,18 +85,42 @@ dropZone.addEventListener('drop', (e) => {
     reader.readAsDataURL(file);
 });
 
-button.addEventListener('click', () => {
+button.addEventListener("click", () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    fetch('https://dog.ceo/api/breeds/image/random')
-        .then(response => response.json())
-        .then(data => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) => {
             imageData = data.message;
             const img = new Image();
             img.src = imageData;
-            img.onload = function() {
+            imageDataOutput = processImage(imageData);
+            img.onload = function () {
                 canvas.width = img.width;
                 canvas.height = img.height;
                 ctx.drawImage(img, 0, 0);
             };
         });
+});
+
+document.getElementById("btn-print").addEventListener("click", function () {
+    window.print();
+});
+
+function processImage(imageData) {
+    //  implementation for processing the image data
+    let processedImageData = imageData;
+    return processedImageData;
+}
+
+
+
+btnConvert.addEventListener("click", () => {
+    ctxOutput.clearRect(0, 0, canvasOutput.width, canvasOutput.height);
+    const img = new Image();
+    img.src = imageDataOutput;
+    img.onload = function () {
+        canvasOutput.width = img.width;
+        canvasOutput.height = img.height;
+        ctxOutput.drawImage(img, 0, 0);
+    };
 });
