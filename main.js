3f8137bccb335e4cdb8cf2c893e68c97bb1199d1
Alon Regev <alonr3704@gmail.com>
2023-02-06 13:39:40 +0200

    add path detection

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function loadImage(){
     let url = document.getElementById("urlInput").value;
     let image = new Image();
     //image.src = "https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_4x3.jpg";
-    image.src = "http://images2.fanpop.com/image/photos/8600000/random-animals-animals-8675984-377-442.jpg";
+    image.src = "https://www.envigo.com/hubfs/resources/model-images/golden-syrian-hamster.png";
     image.setAttribute('crossOrigin', '');
     image.onload = () => {
         let offscreen = new OffscreenCanvas(HEIGHT * image.width / image.height, HEIGHT);
@@ -13,27 +13,82 @@ function loadImage(){
         let context = offscreen.getContext('2d');
         context.drawImage(image, 0, 0, offscreen.width, offscreen.height);
         imageData = context.getImageData(0, 0, offscreen.width, offscreen.height, {colorSpace: "srgb"});
-        process(imageData)
+        let path = process(imageData)
 
         let onscreen = document.getElementById("tempCanvas");
         onscreen.width = offscreen.width;
         onscreen.height = offscreen.height;
-        onscreen.getContext("2d").putImageData(imageData, 0, 0, 0, 0, onscreen.width, onscreen.height);
+        let ctx = onscreen.getContext("2d");
+        ctx.putImageData(imageData, 0, 0, 0, 0, onscreen.width, onscreen.height);
+
+        ctx.beginPath();
+        ctx.moveTo(...path[0]);
+        for(let i = 1; i < path.length; i++){
+            ctx.lineTo(...path[i]);
+        }
+        ctx.stroke();
     };
 }
 
 function process(imageData){
     let w = imageData.width, h = imageData.height;
-    let data = Array(3 * w * h).fill(0);
-    for(let i = 0; i < w; i++)
-        for(let j = 0; j < h; j++)
-            for(let c = 0; c < 3; c++)
-                data[(j * w + i) * 3 + c] = imageData.data[(j * w + i) * 4 + c];
+
+    let data = toRGB(imageData);
+    let opacity = opacityMask(imageData);
+
+    opacity = booleanFilter(opacity, 175);
+    let outline = edgeDetection(opacity, w, h, 1, 1);
+    // outline to list of points
+    let points = [];
+    for(let i = 0; i < outline.length; i++){
+        if(outline[i] == 255)
+            points.push([i % w, Math.floor(i / w)]);
+    }
+    console.log(points);
+    // to path list
+    let path = [points[0]];
+    points.splice(0, 1);
+    while(points.length > 0){
+        let lastPoint = path[path.length - 1];
+        // find closest point
+        let i = getNearestIndex(points, lastPoint);
+        if(sqrDist(lastPoint, points[i]) > 100 ** 2)
+            break;
+        path.push(points[i]);
+        points.splice(i, 1);
+    }
+    // close loop
+    if(sqrDist(path[0], path[path.length - 1]) < 100 ** 2)
+        path.push(path[0]);
+
+    for(let iter = 0; iter < 20; iter++){
+        for(let i = 1; i < path.length - 1; i += 2) {
+            // cos = (a^2+b^2-c^2)/2ab
+            let a2 = sqrDist(path[i - 1], path[i]);
+            let b2 = sqrDist(path[i], path[i + 1]);
+            let c2 = sqrDist(path[i - 1], path[i + 1]);
+            let cos = (a2 + b2 - c2) / (2 * (a2 * b2) ** 0.5);
+            
+            if(cos < -0.9 || c2 < 225)
+                path.splice(i, 1);
+        }
+    }
+
+    console.log(path);
+
+    for(let i = 0; i < imageData.data.length; i++){
+        imageData.data[i] = 255;
+    }
+    for(let i = 0; i < path.length; i++){
+        imageData.data[4 * (path[i][1] * w + path[i][0]) + 1] = 0;
+    }
+
+    return path;
                 
-    data = bilateralFilter_x(data, w, h, 21, 7, 120);
+    /*data = bilateralFilter_x(data, w, h, 21, 7, 120);
     data = bilateralFilter_y(data, w, h, 15, 5, 100);
     
-    /*data = recolorImage(data, 15);
+    data = recolorImage(data, 15);
     data = grayscale(data);
 
     data = edgeDetection(data, w, h, 7);
@@ -41,90 +96,16 @@ function process(imageData){
     data = filter(data, w, h, [gaussianBlurFilter_1d(9)]);
     data = filter(data, w, h, transpose([gaussianBlurFilter_1d(9)]));
 
-    data = booleanFilter(data, 5);*/
+    data = booleanFilter(data, 5);
     data = grayscale(data);
     data = edgeDetection(data, w, h, 3.5);
     data = filter(data, w, h, [gaussianBlurFilter_1d(7)]);
     data = filter(data, w, h, transpose([gaussianBlurFilter_1d(7)]));
     data = booleanFilter(data, 8);
 
+    flood(data, w, h, 85, 85);*/
 
-    for(let i = 0; i < w; i++)
-        for(let j = 0; j < h; j++)
-            for(let c = 0; c < 3; c++)
-                imageData.data[(j * w + i) * 4 + c] = data[(j * w + i) * 3 + c];
-}
-
-function recolorImage(data, k)
-{
-    let centers = KMeansClustering(data, k);
-    console.log(centers);
-    for(let i = 0; i < data.length; i += 3)
-    {
-        let colorIndex = getNearestIndex(centers, data.slice(i, i + 3));
-        for(let j = 0; j < 3; j++)
-            data[i + j] = centers[colorIndex][j];
-    }
-
-    return data;
-}
-
-function KMeansClustering(data, k)
-{
-    let centers = new Array(k);
-    let clusterSums = new Array(k);
-    let clusterSizes = Array(k).fill(0);
-    for(let i = 0; i < k; i++)
-        centers[i] = [Math.random() * 255, Math.random() * 255, Math.random() * 255];
-
-    for(let iteration = 0; iteration < 40; iteration++){
-        for(let i = 0; i < k; i++)
-        {
-            clusterSums[i] = [0, 0, 0];
-            clusterSizes[i] = 0;
-        }
-
-        for(let i = 0; i < data.length; i += 3)
-        {
-            let pixel = data.slice(i, i + 3);
-            // find nearest center index
-            let nearestIndex = getNearestIndex(centers, pixel);
-            for(let j = 0; j < pixel.length; j++)
-            {
-                clusterSums[nearestIndex][j] += pixel[j];
-            }
-            clusterSizes[nearestIndex]++;
-        }
-
-        for(let i = 0; i < centers.length; i++)
-        {
-            if(clusterSizes[i] === 0)
-                centers[i] = [Math.random() * 255, Math.random() * 255, Math.random() * 255];
-            else
-                for(let j = 0; j < centers[i].length; j++)
-                    centers[i][j] = clusterSums[i][j] / clusterSizes[i];
-        }
-    }
-
-    return centers;
-}
-
-function getNearestIndex(points, testPoint)
-{
-    let minDistanceSquared = Number.POSITIVE_INFINITY;
-    let minIndex = 0;
-    for(let i = 0; i < points.length; i++)
-    {
-        let distanceSquared = 0;
-        for(let j = 0; j < testPoint.length; j++)
-            distanceSquared += (points[i][j] - testPoint[j]) ** 2;
-        if(distanceSquared < minDistanceSquared)
-        {
-            minDistanceSquared = distanceSquared;
-            minIndex = i;
-        }
-    }
-    return minIndex;
+    // fromRGB(imageData, data);
 }
 
 loadImage();
