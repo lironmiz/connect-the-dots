fb79904aedb2bf650080b27908af5a3d84c32989
Liron Mizrahi <liri25112003@gmail.com>
2023-02-11 14:52:30 +0200

    feat-style (add gallery-ui) add gallery ui and style

---
+++ Front/js/gallery.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("click",function(e) {
+    if(e.target.classList.contains("gallery-item")){
+        const src = e.target.getAttribute("src");
+        document.querySelector(".modal-img").src = src;
+        const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+        myModal.show();
+    }
+});
