15bbcb708a6fd8db491d6c1e845c1db2ce5f73b1
Alon Regev <alonr3704@gmail.com>
2023-02-11 21:53:27 +0200

    started gallery functionality

--- Front/js/gallery.js
+++ Front/js/gallery.js
@@ -1,5 +1,6 @@
+const SERVER_URL = 'http://localhost:8080';
 
-document.addEventListener("click",function(e) {
+document.addEventListener("click", function(e) {
     if(e.target.classList.contains("gallery-item")){
         const src = e.target.getAttribute("src");
         document.querySelector(".modal-img").src = src;
@@ -7,3 +8,55 @@ document.addEventListener("click",function(e) {
         myModal.show();
     }
 });
+
+let orderBy = 'newest';
+let page = 1;
+let pageCount;
+getPageCount().then(async (count) => {
+    alert(count);
+});
+setPage();
+
+// Get the dropdown button and dropdown menu
+var dropdownButton = document.getElementById("order-dropdown");
+var dropdownMenu = document.querySelector(".dropdown-menu");
+
+
+// store the dropdown menu selection state
+var dropdownItems = document.querySelectorAll(".dropdown-item");
+var selectedOption;
+for (var i = 0; i < dropdownItems.length; i++) {
+    dropdownItems[i].addEventListener("click", function(event) {
+        selectedOption = event.target.textContent.toLowerCase();
+        dropdownMenu.classList.remove("show");
+        document.getElementById('order-dropdown').innerText = selectedOption;
+
+        orderBy = selectedOption;
+        page = 1;
+        setPage();
+    });
+}
+
+// Close the dropdown menu
+window.addEventListener("click", function(event) {
+    if (!event.target.matches('.dropdown-toggle')) {
+        var dropdowns = document.getElementsByClassName("dropdown-menu");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+});
+
+async function setPage(){
+    //fetch(`${SERVER_URL}/images?`)
+}
+
+async function getPageCount(){
+    let response = await fetch(`${SERVER_URL}/pagecount`);
+    let data = response.json();
+    return data.pages;
+}
